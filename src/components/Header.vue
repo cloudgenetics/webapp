5aaef9fb8ef57aad4a2b5e8242b5f0e61dc427b3
Krishna Kumar <krishnak@utexas.edu>
2021-02-07 11:19:09 -0600

    :computer: Header :dart: unit test for header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title class="d-flex align-center">
+      <v-img
+        alt="Logo"
+        class="shrink mr-2"
+        contain
+        src="@/assets/logo.png"
+        transition="scale-transition"
+        width="30"
+      />
+      {{ env.title }}
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+
+  data: () => ({
+    env: {
+      title: process.env.VUE_APP_TITLE
+    }
+  })
+};
+</script>
