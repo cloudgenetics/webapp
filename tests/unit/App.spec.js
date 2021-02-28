7b5505e5183b6ff66b412cd2ddabe3b65d498a05
Krishna Kumar <krishnak@utexas.edu>
2021-02-28 11:28:55 -0600

    :hammer: :dart: Fix unit test with auth0

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -3,16 +3,31 @@ import Vuetify from 'vuetify'
 
 // Components
 import App from '@/App'
+import router from '../src/router'
 
 // Utilities
 import { createLocalVue, mount } from '@vue/test-utils'
+import { Auth0Plugin } from '@/auth'
+import { domain, clientId, audience } from '../../auth_config.json'
 
 describe('App.vue', () => {
   const localVue = createLocalVue()
   let vuetify
 
   beforeEach(() => {
-    vuetify = new Vuetify()
+    vuetify = new Vuetify(Auth0Plugin, {
+      domain,
+      clientId,
+      audience,
+      router,
+      onRedirectCallback: appState => {
+        router.push(
+          appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+        )
+      }
+    })
   })
 
   it('should have company name', () => {
