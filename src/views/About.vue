9c1e60c1c9957a592fdab5f76de5ca7ef67bcabd
Krishna Kumar <krishnak@utexas.edu>
2021-02-28 12:41:46 -0600

    :construction: WIP async fetch from server

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,83 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>External API</h1>
+    <p>
+      You use will use a button to call an external API using an access token,
+      and the API will validate it using the API's audience value.
+      <strong>This route should be private</strong>.
+    </p>
+    <div
+      class="btn-group mt-5"
+      role="group"
+      aria-label="External API Requests Examples"
+    >
+      <button type="button" class="btn btn-primary" @click="callApiEndpoint">
+        Get Public Message
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        @click="callApiSecuredEndpoint"
+      >
+        Get Private Message
+      </button>
+    </div>
+
+    <div v-if="apiMessage" class="mt-5">
+      <h6 class="muted">Result</h6>
+      <div class="container-fluid">
+        <div class="row">
+          <code class="col-12 text-light bg-dark p-4">
+            {{ JSON.stringify(apiMessage, null, 2) }}
+          </code>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { serverUrl } from "../../auth_config.json";
 export default {
   name: "About",
+  data() {
+    return {
+      apiMessage: null
+    };
+  },
+  methods: {
+    async callApiEndpoint() {
+      try {
+        const response = await fetch(`${serverUrl}/api/v1/`, {
+          mode: "no-cors"
+        });
+
+        const textmsg = await response.text();
+        this.apiMessage = textmsg;
+        console.log(response);
+      } catch (e) {
+        this.apiMessage = `Error: the server responded with '${e.response.status}: ${e.response.statusText}'`;
+      }
+    },
+    async callApiSecuredEndpoint() {
+      const accessToken = await this.$auth.getTokenSilently();
+
+      try {
+        const response = await fetch(`${serverUrl}/api/v1/protected/`, {
+          mode: "no-cors",
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+        console.log(accessToken);
+        const json = await response.json();
+        this.apiMessage = json.message;
+      } catch (e) {
+        console.log(e);
+        this.apiMessage = `Error: the server responded with '${e.response.status}: ${e.response.statusText}'`;
+      }
+    }
+  }
 };
 </script>
+
