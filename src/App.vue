ce71811822aaceb962e261051f4c24ef45144c68
Krishna Kumar <krishnak@utexas.edu>
2021-02-28 09:12:51 -0600

    :truck: Migrate appbar to App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <v-app>
-    <AppBar :appbarDrawer.sync="drawer" />
+    <v-app-bar app color="primary" dark clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="d-flex align-center">
+        <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/logo.png"
+          transition="scale-transition"
+          width="30"
+        />
+        {{ env.title }}
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
     <v-navigation-drawer v-model="drawer" app clipped>
       <v-list>
         <v-list-item v-for="[icon, text] in links" :key="icon" link>
@@ -26,12 +40,11 @@
 
 <script>
 import Footer from "@/components/Footer.vue";
-import AppBar from "@/components/AppBar.vue";
 export default {
   name: "App",
 
   data: () => ({
-    details: {
+    env: {
       title: process.env.VUE_APP_TITLE,
       url: process.env.VUE_APP_URL
     },
@@ -43,7 +56,6 @@ export default {
     ]
   }),
   components: {
-    AppBar,
     Footer
   },
   methods: {}
