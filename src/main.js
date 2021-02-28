a253e51b07b8c2762967bfb07dff3d42c318d947
Krishna Kumar <krishnak@utexas.edu>
2021-02-27 18:56:10 -0600

    :construction: :lock: Add auth0 and axios configuration and plugin files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Axios from './plugins/axios'
+import vuetify from './plugins/vuetify'
 import store from './store'
 import router from './router'
+import { Auth0Plugin } from './auth'
+
+import { domain, clientId, audience } from '../auth_config.json'
 
 Vue.config.productionTip = false
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    )
+  }
+})
+
+Vue.use(Axios)
 
 new Vue({
   vuetify,
