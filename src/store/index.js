ae9368811e73e3ea3f5e714ac31b93f9df37732a
Krishna Kumar <krishnak@utexas.edu>
2021-12-28 10:54:49 -0600

    Add vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,34 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+// Authentication modules
+const authmodule = {
+    namespaced: true,
+    state: {
+        // authentication status
+        authStatus: false,
+        accessToken: ''
+    },
+    getters: {
+        getstatus: (state) => {
+            return state.authStatus
+        }
+    },
+    mutations: {
+        activate(state) {
+            state.authStatus = true
+        },
+        deactivate(state) {
+            state.authStatus = false
+        }
+    }
+}
+
+// Store
+const store = new Vuex.Store({
+    modules: {
+        auth: authmodule
+    }
 })
+
+export default store
