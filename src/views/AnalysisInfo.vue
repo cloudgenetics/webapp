748b2532ffc72c8914dc6fb7bdd77c4c0131fb57
Krishna Kumar <krishnak@utexas.edu>
2022-02-20 12:06:41 -0600

    Relayout analysis page

--- src/views/AnalysisInfo.vue
+++ src/views/AnalysisInfo.vue
@@ -1,37 +1,62 @@
 <template>
   <div class="col-md-12 mb-3">
-    <h2>{{ analysis.jobName }}</h2>
+    <h2>{{ job.jobName }}</h2>
+    <v-list>
+      <v-list-item v-for="(info,i) in jobinfo" :key="i">
+        <v-list-item-icon>
+          <v-icon>{{ icons[i] }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{info}}: {{ batchjob[info]}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item :to="/results/ + job.resultsDir">
+        <v-list-item-icon>
+          <v-icon>mdi-chart-line</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title><b>View results:</b> {{job.resultsDir}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
     <p>
       Job script: <br/>
-      <code class="col-12 text-light bg-dark p-4">
-        {{ JSON.stringify(analysis.Command, null, "\t") }}
-      </code>
+      <vue-json-pretty :data="batchjob['Container']['Command']"> </vue-json-pretty>
       <br/>
     </p>
-    <v-btn :to="'/results/' + analysis.resultsDir">
-      <v-icon dark>
-        mdi-card-search
-      </v-icon>
-      view results
-    </v-btn>
   </div>
 </template>
 
 <script>
 import { HTTP } from "@/http-common";
 import { audience, redirectURL } from "../../auth_config.json";
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
 export default {
   name: "AnalysisInfo",
+  components: {
+    VueJsonPretty,
+  },
   data() {
     return {
-      analysis: {}
+      job: {},
+      batchjob: {},
+      icons: ["mdi-clock", "mdi-alarm-check", "mdi-format-list-checks", "mdi-file-chart-outline"],
+      jobinfo: ["StartedAt", "StoppedAt", "Status", "StatusReason"],
+      results: "Results"
     };
   },
+  filters: {
+    pretty: function(value) {
+      return JSON.stringify(JSON.parse(value), null, 2);
+    }
+  },
   created() {
-    this.getAnalysisInfo();
+    this.getJobInfo()
+    this.getBatchJobInfo()
   },
   methods: {
-    async getAnalysisInfo() {
+    async getJobInfo() {
       const accessToken = await this.$auth.getTokenSilently({ audience });
       HTTP.get("jobs/" + this.$route.params.uuid, {
         mode: "cors",
@@ -40,7 +65,21 @@ export default {
           Authorization: `Bearer ${accessToken}`,
         },
       }).then((resp) => {
-        this.analysis = resp.data;
+        this.job = resp.data
+      });
+    },
+    async getBatchJobInfo() {
+      const accessToken = await this.$auth.getTokenSilently({ audience });
+      HTTP.get("batchjobs/" + this.$route.params.uuid, {
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": `${redirectURL}`,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }).then((resp) => {
+        this.batchjob = resp.data["Jobs"][0]
+        this.batchjob["StartedAt"] = new Date(this.batchjob["StartedAt"])
+        this.batchjob["StoppedAt"] = new Date(this.batchjob["StoppedAt"])
       });
     },
   },
